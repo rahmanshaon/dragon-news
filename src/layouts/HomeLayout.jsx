5a2303fe03d627add5b6eee5ded5c44ff4254903
rahmanshaon <rahmanshaon45@gmail.com>
2025-10-18 11:42:42 +0600

    latest news section added

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 const HomeLayout = () => {
   return (
     <>
-      <header>
+      <header className="mt-10">
         <Header />
+        <section className="w-11/12 mx-auto my-5">
+          <LatestNews />
+        </section>
       </header>
       <main>
         <section className="left_nav"></section>
